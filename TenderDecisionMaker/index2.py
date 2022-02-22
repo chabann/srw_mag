eaed64da29e7434bb13431965b38b58ef6eceabf
a.jolobova <a.jolobova@spb.mcart.ru>
2022-02-22 14:53:08 +0300

    Добавлена множественная смена подрядчиков, пересчет штрафов, оплаты, остатка бюджета на проект

--- TenderDecisionMaker/index2.py
+++ TenderDecisionMaker/index2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def draw_difference(x_values, plan_values, color, label):
-    plt.plot(x_values, plan_values, color + '-', label=label)
+    plt.plot(x_values, plan_values, color=color, label=label, marker='_')
 
 
 def sort_dict(dict_cost):
@@ -11,7 +11,7 @@ def sort_dict(dict_cost):
 
 
 def get_project_cost(start_cost):
-    return int(random.randint(int(start_cost), 40) * 10000)
+    return int(random.randint(int(start_cost), int(start_cost) + 3) * 1000)
 
 
 def get_cost(n):
@@ -24,8 +24,8 @@ def get_cost(n):
     return sorted_costs
 
 
-def get_plan(n, items_good, add=0, proportion=1.0):
-    percent = add
+def get_plan(n, items_good, start=0, proportion=1.0):
+    percent = start
     percent_line = []
     n_count = n
     if n_count == 1:
@@ -35,13 +35,9 @@ def get_plan(n, items_good, add=0, proportion=1.0):
         percent_line.append(percent)
 
         if point < int(items_good):
-            percent += (1 / (n_count - 1)) * proportion
+            percent += ((1 - start) / (n_count - 1)) * proportion
         else:
-            percent += (1 / (n_count - 1)) * random.uniform(0.55, 1) * proportion
-            """if point < int(n_count * (items_good / n_count + (1 - items_good / n_count) / 2)):
-                percent += (1 / (n_count - 1)) * random.uniform(0.7, 1) * proportion
-            else:
-                percent += (1 / (n_count - 1)) * random.uniform(0.4, 0.7) * proportion"""
+            percent += ((1 - start) / (n_count - 1)) * random.uniform(0.55, 0.8) * proportion
 
         if percent > 1:
             percent = 1
@@ -53,121 +49,127 @@ def get_next_cost(old_costs, index, number, fact_v):
     return [(old_costs[k][0], int(old_costs[k][1] * (1 - fact_v) * number / index)) for k in range(len(old_costs))]
 
 
-def get_penalty(old_cost, n, i, d, b):
+def get_penalty(old_cost, n, i, f, b, i_k, n_k):
     sigma = random.uniform(1.2, 1.4)
     low_range = []
     old_cost.pop(0)
-    print('old cost', old_cost)
     for comp in old_cost:
-        # print((n-i) * comp[1] / (n - i * (1 - d)))
-        low_range.append(int((n-i) * sigma * comp[1] / (n - i * (1 - d))))
+        low_range.append(int(1 / n * (n - i) * sigma * comp[1] / (1 - f)))
 
-    print('low_range', low_range)
     low_range.sort()
-    print('low_range_sort', low_range)
-    print('low bound', low_range[0] - b * (1 - i/n))
-    print('up bound', low_range[1] - b * (1 - i / n))
-    delta = int(round((low_range[0] + low_range[1] - 2 * b * (1 - i/n)) / 2))
-    print('delta', delta)
+    delta = int(round((low_range[0] + low_range[1] - 2 * b * (1 - i_k / n_k)) / 2))
+    if delta < 0:
+        delta = 0
     return delta
 
 
+def get_tender_data(n, m, d):
+    sorted_costs = get_cost(m)
+    first_pay = get_project_cost(sorted_costs[-1][1] / 1000)
+    p = get_plan(n, n)
+    p_fact = get_plan(n, d * n)
+    return sorted_costs, first_pay, p, p_fact
+
+
 def initialize_parameters():
-    comp_number = 10
     point_number = 20
+    comp_number = 10
     k_break = 0.1
-    sorted_costs = get_cost(comp_number)
-    first_pay = get_project_cost(sorted_costs[0][1] / 10000)
-    p = get_plan(point_number, point_number)
-    p_fact = get_plan(point_number, k_break * point_number)
-    return point_number, comp_number, k_break, sorted_costs, first_pay, p, p_fact
+    return point_number, comp_number, k_break
+
+
+def process_work(last_change, n, cur_cost, plan_, plan_fact, count_workers, costs_, last_stage_done, penalty, balance,
+                 n0):
+    cur_i = last_change
+    for i in range(last_change, n0):
+        cur_i = i
+        if plan_[i] - plan_fact[i] > D:
+
+            finishData[count_workers] = {
+                'timeValues': list(range(last_change, n0)),
+                'planValues': plan_fact[last_change:n0]
+            }
+
+            draw_difference(list(range(last_change, i + 1)), plan_fact[last_change:(i + 1)],
+                            colors[count_workers + 1], 'Компания ' + str(count_workers))
+
+            temp_balance = balance * (1 - (i - last_change) / n)
+            penalty[count_workers] = get_penalty(cur_cost, n0, i, plan_fact[i], temp_balance, i - last_change, n)
+            costs_[count_workers] = round(balance * (i - last_change) / n)  # - penalty[count_workers]  - без штрафов
+            balance = round(balance * (1 - (i - last_change) / n) + penalty[count_workers])
+            last_change = i
+            last_stage_done = plan_fact[i]
+
+            changesWorker[count_workers] = last_change
+            changesWorkerDone[count_workers] = last_stage_done
+            currentBudget[count_workers] = balance
+            break
+    return cur_i, last_stage_done, last_change, costs_, penalty, balance
+
+
+def process_new_tender(plan_, plan_f, n_k, cur_i, d, count):
+    current_cost = []
+    new_cost = get_cost(M)
+    for comp in new_cost:
+        random_coef = random.uniform(1.5, 1.8) * (N - cur_i) / N
+        current_cost.append((comp[0], comp[1] * random_coef))
+
+    new_plan = get_plan(n_k, n_k, plan_f[cur_i])
+    n_plan = plan_[0:cur_i] + [new_plan[_] for _ in range(n_k)]
+
+    if count < 4:
+        new_plan_f = get_plan(n_k + 1, d * (n_k + 1), plan_f[cur_i])
+    else:
+        new_plan_f = get_plan(n_k, n_k, plan_f[cur_i])
+    n_plan_f = plan_f[0:cur_i] + [new_plan_f[_] for _ in range(n_k)]
+
+    if 1 - n_plan_f[-1] < 0.05:
+        n_plan_f[-1] = 1
+    return current_cost, n_plan, n_plan_f
 
 
 if __name__ == "__main__":
-    N, M, k, startCost, firstPay, plan, planFact = initialize_parameters()
-    countWorkers = 1
+    N, M, D = initialize_parameters()  # начальные параметры
+    startCost, budget, plan, planFact = get_tender_data(N, M, D)  # параметры текущего тендера
+    N_k = N
+    changesWorker = {}
+    changesWorkerDone = {}
+    currentBudget = {}
+    countWorkers = 0
+    currentStage = 0
     lastItemChange = 0
     lastStageDone = 0
     finishData = {}
     penalties = {}
-    colors = ['b', 'g', 'y', 'c', 'r']
+    plan_start = plan.copy()
+    colors = ['b', 'g', 'y', 'c', 'red', 'limegreen', 'firebrick', 'coral', 'mediumblue', 'aqua', 'goldenrod',
+              'chocolate', 'magenta', 'teal', 'fuchsia', 'lavender', 'olive', 'lightgreen', 'tan', 'violet',
+              'salmon', 'indigo']
     costs = {}
 
-    firstPay = 14000
+    draw_difference(list(range(N)), plan[0:N], colors[0], 'Плановые показатели')
+    currentBudget[0] = budget
 
-    print('')
-    print('Изначальная стоимость проекта', firstPay)
-    print('')
-    print('Ранжированный список подрядчиков', startCost)
-    print('')
+    while currentStage < (N - 1):
+        countWorkers += 1
+        currentStage, lastStageDone, lastItemChange, costs, penalties, budget = process_work(lastItemChange, N_k,
+                                                                                             startCost, plan,
+                                                                                             planFact, countWorkers,
+                                                                                             costs,
+                                                                                             lastStageDone,
+                                                                                             penalties, budget, N)
+        N_k = N - lastItemChange
+        startCost, plan, planFact = process_new_tender(plan, planFact, N_k, lastItemChange, D, countWorkers + 1)
 
-    currentCost = startCost.copy()
-    planStart = plan.copy()
-    for i in range(N):
-        if plan[i] - planFact[i] > 0.1:
-            print('Контрольная точка ', i)
-            print('Отставание от плана: ', round((plan[i] - planFact[i]) * 100, 1), '%')
-            print('Меняем подрядчика')
-            print('')
+    changesWorkerDone[countWorkers] = planFact[-1]
+    costs[countWorkers] = budget * (N - lastItemChange) / N_k
+    penalties[countWorkers] = 0
 
-            finishData[countWorkers] = {
-                'timeValues': list(range(lastItemChange, i+1)),
-                'planValues': planFact[lastItemChange:i+1]
-            }
+    budget = budget * (1 - (N - lastItemChange) / N_k)
+    currentBudget[countWorkers] = budget
 
-            costs[countWorkers] = int(firstPay * i / N)
-            color_count = countWorkers % 5
-            draw_difference(list(range(lastItemChange, N)), planFact[lastItemChange:N],
-                            colors[color_count], 'Компания ' + str(countWorkers))
-
-            penalties[countWorkers] = get_penalty(currentCost, N, i, planFact[i], firstPay)
-
-            balance = firstPay - costs[countWorkers] + penalties[countWorkers]
-            print('balance', balance)
-            # currentCost = get_next_cost(startCost, N - i, N, planFact[i])
-            currentCost = []
-            newCost = get_cost(M - 1)
-            for comp in newCost:
-                randomCoef = random.uniform(1.5, 1.8) * (N - i) / N
-                currentCost.append((comp[0], comp[1] * randomCoef))
-                # comp[1] *= randomCoef
-
-            # print('Коэффициент надбавки стоимости ', round((1 - planFact[i]) * N / (N - i), 2))
-            print('')
-            print('Новый список подрядчиков', currentCost)
-
-            newPlan = get_plan(N - i, N - i, planFact[i], (1 - planFact[i]))
-            planFact = planFact[0:i] + [newPlan[_] for _ in range(N - i)]
-            plan = plan[0:i] + [newPlan[_] for _ in range(N - i)]
-
-            # balance = int(firstPay - costs[countWorkers] - currentCost[0][1] * (1 - planFact[i]))
-            # if balance < 0:
-            #     penalties[countWorkers] = abs(balance)
-            #else:
-            #     penalties[countWorkers] = 0
-
-            countWorkers += 1
-            lastItemChange = i
-            lastStageDone = planFact[i]
-
-    if 1 - planFact[-1] < 0.05:
-        planFact[-1] = 1
-
-    planFact[-1] = 1
-    if lastItemChange > 0:
-        finishData[countWorkers] = {
-            'timeValues': list(range(lastItemChange, N)),
-            'planValues': planFact[lastItemChange:N]
-        }
-        # costs[countWorkers] = int(currentCost[0][1] * (1 - lastStageDone))
-        costs[countWorkers] = balance
-        color_count = countWorkers % 5
-        draw_difference(list(range(lastItemChange, N)), planFact[lastItemChange:N],
-                        colors[color_count], 'Компания ' + str(countWorkers))
-
-    color_count = countWorkers + 1 % 5
-    draw_difference(list(range(N)), planStart[0:N],
-                    colors[color_count], 'Плановые показатели')
+    draw_difference(list(range(lastItemChange, N)), planFact[lastItemChange:N],
+                    colors[countWorkers + 1], 'Компания ' + str(countWorkers))
 
     plt.title('План выполнения проекта')
     plt.legend()
@@ -178,4 +180,11 @@ if __name__ == "__main__":
     print('Плата подрядчикам: ', costs)
     print('')
     print('Штрафы подрядчикам: ', penalties)
-    # print('finishData', finishData)
+    print('')
+    print('Точки замены: ', changesWorker)
+    print('')
+    print('Выполненная работа: ', changesWorkerDone)
+    print('')
+    print('Остатки бюджета: ', currentBudget)
+    print('')
+
